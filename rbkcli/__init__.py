2569503e538a1bd4b310b1a8c3742d04c7f78113
Bmanesco <bmanesco@gmail.com>
2019-07-25 10:52:16 +0100

    Correct rbkcli package version in ll files

--- rbkcli/__init__.py
+++ rbkcli/__init__.py
@@ -1,7 +1,7 @@
 """Init module for rbkcli"""
 
-__version__ = "1.0.0"
-__author__ = "Bruno Giovanini Manesco"
+__version__ = '1.0.0-beta.0'
+__author__ = 'Bruno Giovanini Manesco'
 __all__ = []
 
 from rbkcli.interface.rbk_cli import RbkCli
